89d002020ae1bef418e9524895f9949ed0395c12
Sergio Perez <sergio13prez@gmail.com>
2019-07-21 16:25:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ function updateObjectWithKeyAndValue(obj, key, value){
     return Object.assign({}, obj,{[key]: value})
 }
 
-function updateObjectWithKeyAndValue2(obj, key, value){
-    return Object.assign({}, obj,{key: value})
-}
-
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
+
+function
