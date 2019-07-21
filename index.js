881d14871e0f8ee92fa78544045462b7194f7579
Sergio Perez <sergio13prez@gmail.com>
2019-07-21 16:17:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var recipes = {food : 'pizza'};
 
 function updateObjectWithKeyAndValue(obj, key, value){
-    return Object.assign({}, obj,{[key]: })
-    
+    return Object.assign({}, obj,{[key]: value})
 }
 
 console.log(updateObjectWithKeyAndValue(recipes, 'prop2', '2'));
